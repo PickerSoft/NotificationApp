f54370677fa68192d4566b9d1b5d2f8883e828f1
pickerdotcom <pickerdotcom@gmail.com>
2022-07-15 14:10:56 +0900

    notification application base.

---
+++ app/src/main/java/com/pickersoft/mynotify/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.pickersoft.mynotify
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.os.Build.VERSION.SDK_INT
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class MainActivity : AppCompatActivity() {
+
+    val CHANNEL_ID = "pickerChannel"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //1st, create notification channel.
+        createNotificationChannel()
+    }
+
+    fun createNotificationChannel() {
+        // Create the notification channel is needed from O OS.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "channel_name"
+            val descriptionText = "description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID,name,importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system.
+            val notificationManager:NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    fun runNotify(view: View) {
+        //2nd, run notify function.
+        var builder = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("this is title")
+            .setContentText("this is content")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notification id should be unique in project.
+            // I just add 0.
+            notify(0,builder.build())
+        }
+    }
+}
